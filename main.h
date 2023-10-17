98dac871395593885191e398c3736481332429c3
Muhammad Hany <dev.mhany@gmail.com>
2023-10-17 04:09:39 +0300

    betty

--- main.h
+++ main.h
@@ -71,7 +71,10 @@ typedef struct format_info fmt_info_t;
 struct spec_printer
 {
 	char specifier;
-	void (*print_arg)(va_list *args, fmt_info_t *fmt_info);
+void (*print_arg)(
+    va_list *args, 
+    fmt_info_t *fmt_info
+);
 };
 typedef struct spec_printer spec_printer_t;
 typedef unsigned char uchar_t;
